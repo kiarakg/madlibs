2c728979d99e8f8a324eff6ab553e625c778810e
Kiara Guzman <kiara.kg11@gmail.com>
2021-06-11 15:41:33 -0400

    madlib paragraph added

--- zombie.py
+++ zombie.py
@@ -4,3 +4,15 @@ def madlib():
     color = input("Color: ")
     verb_past_tense = input("Verb (past tense): ")
     food = input("Food: ")
+    noun1 = input("Noun: ")
+    noun_plural_1 = input("Noun (plural): ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+
+
+    madlib = f"It was a {adj1} summer {time_of_day} when we realized that the vaccine \
+    to stope the spread of the disease did not work. Instead, it produced ZOMBIES! They \
+    were thirsty for {body_part_plural} and the streets were lined with these monsters \
+    holding {noun_plural_1} in their hands. Their eyes were {color} with hunger as they \
+    {verb_past_tense} around the city looking for {food}."
