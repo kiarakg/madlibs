72eba06f1216f90691a99f0f4d03df02adad2937
Kiara Guzman <kiara.kg11@gmail.com>
2021-06-11 15:36:12 -0400

    zombie added

---
+++ zombie.py
@@ -0,0 +1,6 @@
+def madlib():
+    time_of_day = input("Time of day: ")
+    body_part_plural = input("Body Part (plural): ")
+    color = input("Color: ")
+    verb_past_tense = input("Verb (past tense): ")
+    food = input("Food: ")
