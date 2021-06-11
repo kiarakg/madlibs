1e9291047265801ae65d6c171934ea00f9be4df9
Kiara Guzman <kiara.kg11@gmail.com>
2021-06-11 16:15:20 -0400

    zombie paragraph

--- zombie.py
+++ zombie.py
@@ -15,4 +15,8 @@ def madlib():
     to stope the spread of the disease did not work. Instead, it produced ZOMBIES! They \
     were thirsty for {body_part_plural} and the streets were lined with these monsters \
     holding {noun_plural_1} in their hands. Their eyes were {color} with hunger as they \
-    {verb_past_tense} around the city looking for {food}."
+    {verb_past_tense} around the city looking for {food}. There were {adj2} and {adj3}, \
+    unknwing how to act in this human world...except eat {body_part_plural}!! That was \
+    their sole mission and they were dedicated towards achieving it for the sake of {noun1}."
+
+    print(madlib)
